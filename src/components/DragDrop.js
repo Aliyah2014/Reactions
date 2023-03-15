b292f5f5b107d38ce58612b6f270b04c71aab75e
CDS-admin1 <csouthon44@gmail.com>
2023-03-15 16:13:52 +0000

    Adding dragging feature

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -1,4 +1,4 @@
-import React { useState } from 'react';
+import React, { useState } from 'react';
 import { useDrag } from 'react-dnd';
 import Picture from './Pictures';
 
--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useDrag } from 'react-dnd';
 
-function Pictures() {
+
+function Pictures({id, url}) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "image",
+  item: {id: id},
+  collect: (monitor) =>({
+    isDragging: !!monitor.isDragging(),
+  }),
+ }));
   return (
-    <div>
-      
-    </div>
-  )
+    <img
+    ref={drag}
+    src={url}
+    width="150px"
+    alt=""
+    style={{border: isDragging ? "5px solid black" : "0px"}} />;
+ 
 }
 
 export default Pictures;
