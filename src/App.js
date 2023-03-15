7abb010b0faa449922c02aa8162a1974f0f5a9bc
CDS-admin1 <csouthon44@gmail.com>
2023-03-15 16:19:57 +0000

    Added DndProvider to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DragDrop from './components/DragDrop';
 import './App.css';
 
 function App() {
   return (
+    <DndProvider backend={HTML5Backend}>
     <div className="App">
+    <DragDrop />
       <header className="App-header">
       </header>
     </div>
+    </DndProvider>
   );
 }
 
