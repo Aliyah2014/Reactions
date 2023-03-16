aa732b9e8c69f1531f18e3b505d075d935427595
CDS-admin1 <csouthon44@gmail.com>
2023-03-16 10:00:32 +0000

    Adding NavBar and Buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import DragDrop from './components/DragDrop';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
     <div className="App">
+    <NavBar />
     <DragDrop />
       <header className="App-header">
+        
       </header>
     </div>
     </DndProvider>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function BasicExample() {
+function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        {/* Add image rather than text to brand */}
+        <Navbar.Brand href="#home">Reacti😄ns</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            {/* <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -23,7 +26,23 @@ function BasicExample() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
+            <div>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+            </div>
+            <div className="ButtonDiv">
+            <Button className="rounded-pill m-1" variant="secondary" size="lg">+ Widgets</Button>
+            
+            <Button className="rounded-pill m-1" variant="secondary" size="lg">↑ Upload</Button>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -31,4 +50,4 @@ function BasicExample() {
   );
 }
 
-export default BasicExample;
+export default NavBar;
