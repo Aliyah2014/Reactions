978869a76cb7efdbe5c7a4253613a49485e74faa
CDS-admin1 <csouthon44@gmail.com>
2023-03-16 11:28:12 +0000

    Removed MainBoard files

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import DragDrop from './components/DragDrop';
 import NavBar from './components/NavBar';
 import './App.css';
-import MainBoard from './components/MainBoard';
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
     <div className="App">
     <NavBar />
-    <MainBoard />
-    <DragDrop />
-      <header className="App-header">
+      <DragDrop />
+        <header className="App-header">
         
-      </header>
+        </header>
     </div>
     </DndProvider>
   );
