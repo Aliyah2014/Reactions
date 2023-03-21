30b0e5023c0a8ede88a64d7d2666dc84761bded0
CDS-admin1 <csouthon44@gmail.com>
2023-03-21 12:39:27 +0000

    Creating Api call function in SearchResults

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from "react";
+import SearchBox from "./SearchBox";
+import ImageResults from "./ImageResults";
 import API from "../utils/API";
+
+function SearchResults() {
+
+    
 
 const [img, setImg] = useState("");
 const [res, setRes] = useState([]);
 
-function SearchResults() {
+// const for api
+
+// const for handling results
+
+// const for submitting search
+
   return (
     <div>
       
