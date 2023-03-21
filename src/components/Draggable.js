075a20c9639cc90f5ed7330fb9d622abfe969035
Benjamin Wild <103186240+itsemtuk@users.noreply.github.com>
2023-03-21 16:36:18 +0000

    update to dndkit

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { useDraggable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 
 export function Draggable(props) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: "draggable",
   });
-  const style = transform
-    ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  const style = 
+     {
+        transform: CSS.Translate.toString(transform), 
       }
-    : undefined;
+    
 
   return (
     <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
       {props.children}
     </button>
   );
+
+  
 }
 
 export default Draggable;
